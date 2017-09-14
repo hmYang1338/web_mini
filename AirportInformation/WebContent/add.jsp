315a0aea91731ac9becc6c98a9f5f8cd08fc3db6
jeong-gyukim <jeong-gyukim@218.38.137.28>
2017-09-14 17:25:03 +0900

    start_2

---
+++ AirportInformation/WebContent/add.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>add.jsp</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Navigation Bar -->
+<div class="w3-bar w3-white w3-large">
+  <a href="index.html" class="w3-bar-item w3-button w3-red w3-mobile">Main</a>
+  
+  <a href="add.jsp" class="w3-bar-item w3-button w3-right w3-mobile">Join</a>
+  <a href="login.jsp" class="w3-bar-item w3-button w3-right w3-mobile">Login</a>
+</div>
+</head>
+<body>
+	회원가입 할래요
+</body>
+</html>
---
+++ AirportInformation/src/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package dto;
+
+public class UserDTO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		super();
+	}
+	public UserDTO(String id, String pwd, String name, String email) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("UserDTO [id=");
+		builder.append(id);
+		builder.append(", pwd=");
+		builder.append(pwd);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
