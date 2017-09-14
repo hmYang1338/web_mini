f5bed71ab58c5e8b7b3b017a41541607b2c3ba2b
Hyunmo Yang <moo1338@naver.com>
2017-09-15 08:23:51 +0900

    ADD  // userDAO & context.html

--- AirportInformation/src/dao/UserDAO.java
+++ AirportInformation/src/dao/UserDAO.java
@@ -1,11 +1,21 @@
-package dao;
+﻿package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import dto.UserDTO;
+import util.DBUtil;
 
 /**
  * 회원가입, 회원탈퇴, 회원수정, 로그인 기능, 내정보 보기
  */
 public class UserDAO {
 	private static UserDAO userDAO;
-
+	private static DataSource source = null;
+	
 	private UserDAO() {
 	}
 
@@ -18,32 +28,119 @@ public class UserDAO {
 
 	/**
 	 * 회원가입
+	 * @throws SQLException 
 	 */
-	public void userInsert() {
+	public static void userInsert(UserDTO udo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "INSERT INTO users VALUES(?,?,?,?)";
+		try {
+			con = source.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, udo.getId());
+			pstmt.setString(2, udo.getPwd());
+			pstmt.setString(3, udo.getName());
+			pstmt.setString(4, udo.getEmail());
+			pstmt.executeUpdate();
+		} catch (SQLException s) {
+			s.printStackTrace();
+			throw s;
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
 	}
 
 	/**
 	 * 회원 탈퇴
+	 * @throws SQLException 
 	 */
-	public void userDelete() {
+	public static void userDelete(String id, String email) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "DELETE FROM users WHERE id = ? && email = ?";
+		try {
+			con = source.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, email);
+			pstmt.executeUpdate();
+		} catch (SQLException s) {
+			s.printStackTrace();
+			throw s;
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
 	}
 
 	/**
 	 * 회원 수정
+	 * @throws SQLException 
 	 */
-	public void userUpdate() {
+	public static void userUpdate(UserDTO udo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "UPDATE users SET password = ? , email = ? WHERE id = ?";
+		try {
+			con = source.getConnection();
+
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, udo.getPwd());
+			pstmt.setString(2, udo.getEmail());
+			pstmt.setString(3, udo.getId());
+
+			pstmt.executeQuery();
+
+		} catch (SQLException s) {
+			s.printStackTrace();
+			throw s;
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
 	}
 
 	/**
 	 * 로그인
+	 * @throws SQLException 
 	 */
-	public void userLogin() {
+	public void userLogin(UserDTO udo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "SELECT id, pwd FROM users WHERE id = ? ";
+		try {
+			con = source.getConnection();
+			
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, udo.getId());
+			pstmt.executeQuery();
+		} catch (SQLException s) {
+			s.printStackTrace();
+			throw s;
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
 	} 
 
 	/**
 	 * 내정보 보기
+	 * @throws SQLException 
 	 */
-	public void userInfo() {
-	}
+	public void userInfo(UserDTO udo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "SELECT * FROM users WHERE id = ? ";
+		try {
+			con = source.getConnection();
 
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, udo.getId());
+
+			pstmt.executeQuery();
+
+		} catch (SQLException s) {
+			s.printStackTrace();
+			throw s;
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+	}
 }
