2368e1025273fd41f678e0d98c1211035c9c90d0
Hyunmo Yang <moo1338@naver.com>
2017-09-15 18:22:59 +0900

    18:22

--- AirportInformation/src/service/BookingService.java
+++ AirportInformation/src/service/BookingService.java
@@ -1,11 +1,15 @@
 package service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.BookingInfoDAO;
 
 @WebServlet("/booking")
 public class BookingService extends HttpServlet {
@@ -17,11 +21,20 @@ public class BookingService extends HttpServlet {
 		
 	} // end of service
 	
+	
 	public void bookingInfo(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String id = request.getParameter("id");
-		String bookingCode = request.getParameter(") 
-		
+		String bookingCode = request.getParameter("bookingCode").trim();
+		String id = request.getParameter("id").trim();
+		String AFlightID = request.getParameter("AFlightID").trim();
+		String AScheduledateTime = request.getParameter("AScheduledateTime").trim();
+		String DFlightID = request.getParameter("DFlightID").trim();
+		String DScheduledateTime = request.getParameter("DScheduledateTime").trim();
+	
+		ArrayList<String> list = null;
+		/*try {
+			list = BookingInfoDAO.searchBooking(bto);
+		}*/
 	}
 
 }
